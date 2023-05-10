f1afae493082ba2112026dfead5bf922e6625c2f
Mus1ak <mustakch27@gmail.com>
2023-05-10 23:13:15 +0530

    second-commit

---
+++ Variables&Datatypes/Vd.java
@@ -0,0 +1,21 @@
+public class Vd {
+    public static void main(String[] args) {
+        //variables - variables are like containers that store some entity, like for example we take a real life example , your mom asks you to store the sugar that she bought from the market to a "Dabba" or a container in general words. so here we can see that we are storing that entity ((sugar)) in a dabba or container thats what a variable is.
+
+        //Datatypes - datatypes are just some types of data like sugar is solid, soft drink is liquid, just like that in programming terms we have integers for numbers , strings for like words we can say and so on...
+
+        //we have basically two datatypes (Types):
+        // 1. Primitive Datatypes : these are the datatypes that cannot be further divided to other smaller datatypes. like int cant be divided to other datatype.
+
+        // 2. non-Primitive Datatypes : these are the datatypes that we can further divided to other smaller datatypes. like array it can be divided to a single int. or lets say a string it can be divided to single characters. {STRING : "S", "T", "R", "I", "N", "G"}
+
+        //examples
+        int a = 45;
+        float b = 2.5f;
+        double c = 23213123;
+        long d = 21124214124L;
+        char e = 'M';
+
+        System.out.println("The Variables in the example were a b c d e and their respective values that i gave were :"+a+","+b+","+c+","+d+","+e);
+    }
+}
