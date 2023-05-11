784e5f65f5006b196e2e66556eb4b786a5fe5fa2
Mus1ak <mustakch27@gmail.com>
2023-05-11 15:43:40 +0530

    third2-commit

---
+++ TakingInput/swap_num/swapnum.java
@@ -0,0 +1,16 @@
+//program to swap numbers - take two numbers from the user and swap it.
+
+
+import java.util.Scanner;
+
+public class swapnum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("swapped values of a & b:"+a+" "+b);
+    }
+}
