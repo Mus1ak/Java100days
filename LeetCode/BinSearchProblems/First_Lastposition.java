4b10fc0621ba9adee60294c59004a4457649c239
Mus1ak <mustakch27@gmail.com>
2023-05-19 22:16:50 +0530

    Binary Search in 2D arrays

--- LeetCode/BinSearchProblems/First_Lastposition.java
+++ LeetCode/BinSearchProblems/First_Lastposition.java
@@ -3,24 +3,84 @@ import java.util.Arrays;
 public class First_Lastposition {
     public static void main(String[] args) {
         int nums[] = { 1, 6, 7, 7, 8, 9, 10 };
-        int target = 12;
+        int target = 7;
         int pos[] = Search(nums, target);
         System.out.println(Arrays.toString(pos));
     }
+    /*
+    static int[] Search(int[] nums, int target) {
+    int fP = -1;
+    int lP = -1;
+    int i = 0;
+    while (i < nums.length) {
+    if (nums[i] == target) {
+    if (fP == -1) {
+    fP = i;
+    }
+    lP = i;
+    }
+    i++;
+    }
+    return new int[] { fP, lP };
+    }
+     */
+    /*
+    Better time complexity method is using binary search():
+    - first as it says find the first position and last position we will divide
+    it and will find the first position of the element first and then the last
+    position using binary search O(logN) time complexity
+    - so when element is found we will check if theres any other element that is
+    on the left side for the first position so will do : e = mid - 1
+    - similarly for last position after finding the index of the element we will
+    check for any other possible position of that element on the right side by
+    doing : s = mid+1
+    - rest of the code is same as binary search.
+    --------------------------------------------------------------------
+     * Code :
+     */
 
     static int[] Search(int[] nums, int target) {
-        int fP = -1;
-        int lP = -1;
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] == target) {
-                if (fP == -1) {
-                    fP = i;
-                }
-                lP = i;
-            }
-            i++;
-        }
+        int fP = binSearchL(nums, target);
+        int lP = binSearchR(nums, target);
         return new int[] { fP, lP };
     }
+
+    // function to find the left most or first position of the element
+    static int binSearchL(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                e = mid - 1;
+            } else if (arr[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // function to find the right most or last position of the element
+    static int binSearchR(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                s = mid + 1;
+            } else if (arr[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
 }
--- LeetCode/BinSearchProblems/SRotatedarray.java
+++ LeetCode/BinSearchProblems/SRotatedarray.java
@@ -1,41 +1,86 @@
+// public class SRotatedarray {
+//     public static void main(String[] args) {
+//         int nums[] = {5,1,3};
+//         // int nums[] = {4,5,6,7,0,1,2};
+//         int target = 5;
+//         System.out.println(Search(nums, target));
+//     }
+
+//     static int peakElement(int[] nums) {
+//         int s = 0;
+//         int e = nums.length - 1;
+//         while (s < e) {
+//             int mid = s + (e - s) / 2;
+//             if (nums[mid] > nums[e]) {
+//                 s = mid + 1;
+//             } else {
+//                 e = mid;
+//             }
+//         }
+//         return s;
+//     }
+
+//     static int Search(int[] nums, int target) {
+//         int peakE = peakElement(nums);
+
+//         if (nums[peakE] == target) {
+//             return peakE;
+//         }
+//         int f = binSearch(nums, target, 0, peakE);
+//         if (f != -1) {
+//             return f;
+//         }
+//         return binSearch(nums, target, peakE + 1, nums.length - 1);
+//     }
+
+//     static int binSearch(int[] nums, int target, int s, int e) {
+//         while (s <= e) {
+//             int mid = s + (e - s) / 2;
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] < target) {
+//                 s = mid + 1;
+//             } else {
+//                 e = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+/* Second Approach */
+
 public class SRotatedarray {
     public static void main(String[] args) {
-        int nums[] = {5,1,3};
-        // int nums[] = {4,5,6,7,0,1,2};
-        int target = 5;
-        System.out.println(Search(nums, target));
-    }
+        int arr[] = { 3,4,5,8,0,1,2 };
+        int target = 0;
 
-    static int peakElement(int[] nums) {
-        int s = 0;
-        int e = nums.length - 1;
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] > nums[e]) {
-                s = mid + 1;
-            } else {
-                e = mid;
-            }
-        }
-        return s;
+        // search for the pivot element
+        // System.out.println(Pivot(arr));
+        System.out.println(Search(arr, target));
     }
 
     static int Search(int[] nums, int target) {
-        int peakE = peakElement(nums);
+        int pivotelement = Pivot(nums, 0, nums.length - 1);
 
-        if (nums[peakE] == target) {
-            return peakE;
+        if(nums.length == 1 && nums[0] == target){
+            return 0;
         }
-        int f = binSearch(nums, target, 0, peakE);
-        if (f != -1) {
-            return f;
+
+        if (nums[pivotelement] == target) {
+            return pivotelement;
         }
-        return binSearch(nums, target, peakE + 1, nums.length - 1);
+        if (pivotelement == -1) {
+            return binSearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[0] <= target) {
+            return binSearch(nums, target, 0, pivotelement - 1);
+        }
+        return binSearch(nums, target, Pivot(nums, 0, nums.length - 1) + 1, nums.length - 1);
     }
 
     static int binSearch(int[] nums, int target, int s, int e) {
         while (s <= e) {
-            int mid = s + (e - s) / 2;
+            int mid = (s + e) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
@@ -45,5 +90,28 @@ public class SRotatedarray {
             }
         }
         return -1;
+    }
+
+    static int Pivot(int nums[], int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int mid = (s + e) / 2;
+        // case 1: mid > m+1 in that case mid is the pivot
+        if (mid < e && nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+        // case 2: mid < mid - 1 means mid - 1 is pivot
+        else if (mid > s && nums[mid] < nums[mid - 1]) {
+            return mid - 1;
+        }
+        // case 3
+        else if (nums[mid] <= s) {
+            Pivot(nums, s, mid - 1);
+        }
+
+        // case 4
+
+        return Pivot(nums, mid + 1, e);
     }
 }
