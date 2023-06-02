bfaf49c3c271dcdc170dc1abe85a8e706cf2bf10
Mus1ak <mustakch27@gmail.com>
2023-06-02 20:59:25 +0530

    third5-commit

--- Sorting/BubbleSorting/bubbleSort.java
+++ Sorting/BubbleSorting/bubbleSort.java
@@ -16,7 +16,7 @@ public class bubbleSort {
         Worst Case = O(N^2)
     */
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
+        int arr[] = {2,32,41,56,1,3};
         BubbleS(arr);
         System.out.println(Arrays.toString(arr));
     }
