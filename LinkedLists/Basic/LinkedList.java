8a27cb46bf7e4b7d9fa7ee60465e806d769e9ef7
Mus1ak <mustakch27@gmail.com>
2024-01-18 21:20:40 +0530

    learning linkedlist

--- LinkedLists/Basic/LinkedList.java
+++ LinkedLists/Basic/LinkedList.java
@@ -12,6 +12,7 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //medthods - add, remove etc
 //--------------------------------------------------------------------------
@@ -21,7 +22,7 @@ public class LinkedList {
 
         //base case: if the the ll is empty then create a node and mark it as head and tail
         if (head == null) {
-            head = newNode = tail;
+            head = tail = newNode;
             return;
         }
         //point new node's next to the head
@@ -38,7 +39,7 @@ public class LinkedList {
         Node newNode2 = new Node(data);
         //base case - if linkedlist is empty
         if(head == null){
-            head = newNode2 = tail;
+            head = tail = newNode2;
             return;
         }
 
@@ -48,6 +49,27 @@ public class LinkedList {
         //change tail
         tail = newNode2;
     }
+
+
+    public void addinMiddle(int idx, int data){
+
+        if (idx == 0) {
+            addFirst(data);
+        }
+
+        Node newNode3 = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        //when i == idx - 1
+        newNode3.next = temp.next;
+        temp.next = newNode3;
+    }
+
 //--------------------------------------------------------------------------
     public void printLL(){
         if(head == null){
@@ -63,6 +85,34 @@ public class LinkedList {
         System.out.println("null");
     }
 
+
+    public int removeFirst(){
+
+        if (size == 1) {
+            head = tail = null;
+            return Integer.MIN_VALUE;
+        }
+
+        int value = head.data;
+        head = head.next;
+
+        return value;
+    }
+
+
+    public int itrSearch(int key){
+        int i = 0;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
 //------------------------Main Driver code------------------------------
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
@@ -72,7 +122,11 @@ public class LinkedList {
         //Add last
         ll.addLast(3);
         ll.addLast(4);
+        //add in the middle
+        ll.addinMiddle(2, 7);
         //Print
         ll.printLL();
+
+       System.out.println( ll.itrSearch(9));
     }
 }
