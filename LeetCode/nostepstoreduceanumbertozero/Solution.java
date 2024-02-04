57f391c6f9436837dcef355e8f014291efaad2e4
Mus1ak <mustakch27@gmail.com>
2024-02-04 21:24:10 +0530

    leetcode easy problem

---
+++ LeetCode/nostepstoreduceanumbertozero/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static void main(String[] args) {
+        int num = 14;
+        System.out.println(numberOfSteps(num));
+    }
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        
+        
+        if (num % 2 == 1) {
+            num = num - 1;
+            count++;
+        }
+        
+        if (num == 0) {
+            return count;
+        }
+        count++; 
+        return count + numberOfSteps(num / 2);
+    }
+}
