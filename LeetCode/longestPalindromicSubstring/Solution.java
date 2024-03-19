fa6c76ce5fe79c3fa073ff948f33035e20730a0b
Mus1ak <mustakch27@gmail.com>
2024-03-19 22:14:08 +0530

    solved a  medium level leetcode question

--- LeetCode/longestPalindromicSubstring/Solution.java
+++ LeetCode/longestPalindromicSubstring/Solution.java
@@ -1,17 +1,44 @@
+
+import java.util.Collections;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(LongPalSubstr("abababa"));
+        System.out.println(LongPalSubstr("cbbd"));
     }
     public static String LongPalSubstr(String s){
-        int start = 0;
-        int end = s.length() - 1;
-        String temp = s.substring(start, end);
-        // while (start<=end) {
-        //     // if(temp.charAt(start) == temp.charAt(end)){
+        if(s.length() <= 1){
+            return s;
+        }
 
-        //     // }
-        // } 
-        System.out.println(temp);
-        return "";
+        String LPS = "";
+
+        for (int i = 1; i < s.length(); i++) {
+            int low = i;
+            int high = i;
+            while (s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+
+                if(low == -1 || high == s.length()) break;
+            }
+
+            String palindrome = s.substring(low+1, high);
+            if(palindrome.length() > LPS.length()) LPS = palindrome;
+
+            //even 
+            low = i-1;
+            high = i;
+            while (s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+
+                if(low == -1 || high == s.length()) break;
+            }
+            palindrome = s.substring(low+1, high);
+            if(palindrome.length() > LPS.length()) LPS = palindrome;
+
+        }
+        return LPS;
     }
+
 }
