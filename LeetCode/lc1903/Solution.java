6836462a535dbfc13ee491f00f3d3cd2d8e6a3aa
Mus1ak <mustakch27@gmail.com>
2024-03-22 14:24:02 +0530

    solved a  easy level leetcode question.

--- LeetCode/lc1903/Solution.java
+++ LeetCode/lc1903/Solution.java
@@ -2,25 +2,20 @@ import java.util.Arrays;
 
 class Solution{
     public static void main(String[] args) {
-        String num = "4206";
+        String num = "7542351161";
         System.out.println(largestOddNumber(num));
     }
     public static String largestOddNumber(String num) {
-        String[] nums = num.split("");
-        int max = 0;
         
-        int arr[] = new int[num.length()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(nums[i]);
-        }
+        int max = Integer.MIN_VALUE;
+        
+        int number = Integer.parseInt(num); //converting String to number
 
-        for (int n : arr) {
-            if(n%2!=0 && n>max){
-                max = n;
-                return String.valueOf(max);
-            }
+        while(number>0){
+            if(number%2!=0 && number>max) return String.valueOf(number); //converting number to string
+            number /= 10;
         }
-
+        
         return "";
     }
 }
