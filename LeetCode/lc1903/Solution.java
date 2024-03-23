6789b49aca03139c8fa108b89d4c2579b1dc63fb
Mus1ak <mustakch27@gmail.com>
2024-03-23 13:39:29 +0530

    solved a  easy level leetcode question.

--- LeetCode/lc1903/Solution.java
+++ LeetCode/lc1903/Solution.java
@@ -7,9 +7,10 @@ class Solution{
     }
     public static String largestOddNumber(String num) {
         
-        int max = Integer.MIN_VALUE;
+        long max = Integer.MIN_VALUE;
         
-        int number = Integer.parseInt(num); //converting String to number
+        // int number = Integer.parseInt(num); //converting String to number
+        long number = Long.parseLong(num);
 
         while(number>0){
             if(number%2!=0 && number>max) return String.valueOf(number); //converting number to string
