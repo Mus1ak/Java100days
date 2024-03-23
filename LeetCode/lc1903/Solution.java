8400090e8dfd072c292971d465ac554cabedc80a
Mus1ak <mustakch27@gmail.com>
2024-03-23 14:00:20 +0530

    solved a  easy level leetcode question.

--- LeetCode/lc1903/Solution.java
+++ LeetCode/lc1903/Solution.java
@@ -2,15 +2,21 @@ import java.util.Arrays;
 
 class Solution{
     public static void main(String[] args) {
-        String num = "7542351161";
+        String num = "524290740297509725075247274982472874029749";
         System.out.println(largestOddNumber(num));
     }
     public static String largestOddNumber(String num) {
         
-        long max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         
-        // int number = Integer.parseInt(num); //converting String to number
-        long number = Long.parseLong(num);
+        String[] numarr = num.split("");
+        if(Integer.parseInt(numarr[numarr.length-1]) % 2 != 0){
+            return num;
+        }
+
+        int number = Integer.parseInt(num); //converting String to number
+        // long number = Long.parseLong(num);
+        
 
         while(number>0){
             if(number%2!=0 && number>max) return String.valueOf(number); //converting number to string
