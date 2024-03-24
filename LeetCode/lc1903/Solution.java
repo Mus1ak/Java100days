f523fe55dd04388c72118702b9516265f1ddea27
Mus1ak <mustakch27@gmail.com>
2024-03-24 12:21:33 +0530

    solved a  easy level leetcode question and linkedlist

--- LeetCode/lc1903/Solution.java
+++ LeetCode/lc1903/Solution.java
@@ -2,27 +2,39 @@ import java.util.Arrays;
 
 class Solution{
     public static void main(String[] args) {
-        String num = "524290740297509725075247274982472874029749";
+        String num = "29283276444787797987978999999999777995526";
         System.out.println(largestOddNumber(num));
     }
     public static String largestOddNumber(String num) {
         
-        int max = Integer.MIN_VALUE;
+        // long max = Integer.MIN_VALUE;
         
-        String[] numarr = num.split("");
-        if(Integer.parseInt(numarr[numarr.length-1]) % 2 != 0){
-            return num;
-        }
+        // String[] numarr = num.split("");
+        // if(Integer.parseInt(numarr[numarr.length-1]) % 2 != 0 || Integer.parseInt(numarr[numarr.length-2]) % 2 != 0 || Integer.parseInt(numarr[numarr.length-3]) % 2 != 0){
+        //     return num;
+        // }
 
-        int number = Integer.parseInt(num); //converting String to number
-        // long number = Long.parseLong(num);
+        // long number = Long.parseLong(num); //converting String to number
+        // // long number = Long.parseLong(num);
         
 
-        while(number>0){
-            if(number%2!=0 && number>max) return String.valueOf(number); //converting number to string
-            number /= 10;
-        }
+        // while(number>0){
+        //     if(number%2!=0 && number>max) return String.valueOf(number); //converting number to string
+        //     number /= 10;
+        // }
         
+        // return "";
+
+
+        //optimized
+        for (int i = num.length() - 1; i >= 0; i--) {
+            // Convert the character at the current index to an integer
+            int dig = num.charAt(i) - '0';
+
+            if((dig & 1) == 1){
+                return num.substring(0, i+1);
+            }
+        }
         return "";
     }
 }
--- LinkedLists/Basic/LinkedList.java
+++ LinkedLists/Basic/LinkedList.java
@@ -26,7 +26,7 @@ public class LinkedList {
             return;
         }
         //point new node's next to the head
-        newNode.next = head; //Lining of nodes
+        newNode.next = head; //Linking of nodes
 
         //change head to this node
         head = newNode;
@@ -111,22 +111,110 @@ public class LinkedList {
             i++;
         }
         return -1;
+    }   
+
+    //function to reverse a linked list (iterative method - using loop)
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
+    public void removeNthNodefromEnd(int N){
+        //calculate size
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+
+        //if the nth node is basically the first node then just remove the head or we can say remove first operation will be performed
+        if(N == sz){
+            head = head.next;
+            return; 
+        }
+
+        //remove nth node from end is basically remove -> (size - n + 1)th node from start so we will go till the prev node of that nth node and then just change its next to the next of next
+        Node prev = head;
+        int i = 1;
+        while (i < sz - N) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+    }
+
+
+    //slow fast apporoach - turtle rabbit approach
+    public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+
+        return slow; //slow is the mid
+
+    }
+
+    public boolean checkPalindrome(){
+        //edge case if ll is empty or ll has only one node -> so its def a palindrome
+        if(head == null || head.next == null) return true;
+        //1. find mid
+        Node midNode = findMid(head);
+        //2. reverse the 2nd half 
+        Node curr = midNode;
+        Node prev = null;
+        
+        while (curr!=null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        //3. check if the right is equal to left
+        while (right != null) {
+            if(left.data != right.data) return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
 //------------------------Main Driver code------------------------------
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         //Add first
-        ll.addFirst(2);
         ll.addFirst(1);
-        //Add last
-        ll.addLast(3);
-        ll.addLast(4);
-        //add in the middle
-        ll.addinMiddle(2, 7);
-        //Print
-        ll.printLL();
+        ll.addFirst(1);
+        ll.addFirst(1);
+        ll.addFirst(1);
 
-       System.out.println( ll.itrSearch(9));
+        
+        //Add last
+        // ll.addLast(3);
+        // ll.addLast(1);
+        //add in the middle
+        // ll.addinMiddle(2, 7);
+        //Print
+        // ll.reverse();
+        // ll.printLL();
+        
+        // ll.removeNthNodefromEnd(5);
+        ll.printLL();
+        System.out.println(ll.checkPalindrome());
+
+    //    System.out.println( ll.itrSearch(9));
     }
 }
