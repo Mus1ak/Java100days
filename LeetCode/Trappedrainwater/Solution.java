7896a8ce14ff6eb8c735d558ae9b35cfb0429a5a
Mus1ak <mustakch27@gmail.com>
2024-03-25 14:28:10 +0530

    solved medium level question leetcode question  named Trapped rain water

--- LeetCode/Trappedrainwater/Solution.java
+++ LeetCode/Trappedrainwater/Solution.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int height[] = {4,2,0,6,3,2,5};
+        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trapRain(height));
     }
 
