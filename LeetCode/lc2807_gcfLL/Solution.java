973c84f8caa2b985043c5e90a61eab78388f1b91
Mus1ak <mustakch27@gmail.com>
2024-04-07 20:38:31 +0530

    linkedlist medium level question

---
+++ LeetCode/lc2807_gcfLL/Solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+
+    public class ListNode {
+            int val;
+            ListNode next;
+            ListNode() {}
+            ListNode(int val) { this.val = val; }
+            ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
+    public static void main(String[] args) {
+        
+    }
+
+    public static int findgcf(int a, int b){
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static ListNode insertGreatestCommonDivisors(ListNode head) {
+        //size
+        ListNode temp = head;
+        int size = 0;
+        while(temp != null){
+            size++;
+            temp = temp.next;
+        }
+        
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            ListNode newNode = new ListNode(findgcf(curr.val, curr.next.val));
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next; 
+        } 
+    return head;
+    }
+}
