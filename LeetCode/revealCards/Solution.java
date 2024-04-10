da63907cd5a48aa4236f010afb9c81b86626c1c3
Mus1ak <mustakch27@gmail.com>
2024-04-10 15:40:11 +0530

    leetcode daily question

---
+++ LeetCode/revealCards/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int deck[] = {17,13,11,2,3,5,7};
+        System.out.println(Arrays.toString(deckRevealedIncreasing(deck)));
+    }
+    public static int[] deckRevealedIncreasing(int[] deck) {
+        //reveal top card - remove queue
+        //if theres more card below it then move that card to the last
+        Arrays.sort(deck);
+        int revealed[] = new int[deck.length];
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<deck.length; i++){
+            q.add(i);
+        }
+        for (int card : deck) {
+            revealed[q.poll()] = card; 
+            if (!q.isEmpty()) {
+                q.add(q.poll());
+            }
+        }
+        return revealed;
+    }
+}
