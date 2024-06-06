d5683a4dd551e66afe49a061cbafdff9f767d277
Mus1ak <mustakch27@gmail.com>
2024-06-06 11:32:39 +0530

    recursion subsq whose sum is k

--- Recursion/sqwhoseSumisK.java
+++ Recursion/sqwhoseSumisK.java
@@ -1,34 +1,44 @@
+import java.util.*;
+
 class sqwhoseSumisK {
     public static void main(String[] args) {
+
         int nums[] = {1,1,2,3};
         System.out.println(numSubseq(nums, 4));
     }
 
-    public static int numSubseq(int[] nums, int target) {
+    public static List<List<Integer>> numSubseq(int[] nums, int target) {
+        List<List<Integer>> output = new ArrayList<>(); 
         int sum = 0;
         int ind = 0;
-        return helper(nums, target, sum, ind);
+        helper(nums, target, sum, ind, output, new ArrayList<>());
+        return output;
     }
 
-    private static int helper(int nums[], int target, int sum, int ind){
+    private static void helper(int nums[], int target, int sum, int ind, List<List<Integer>> output, List<Integer> res){
         if(sum > target){
-            return 0;
+            return;
         }
         if(ind == nums.length){
             if(sum == target){
-                return 1;
-            }else return 0;
+                //adding the copy of res to output as lists in java are mutable they can be changed
+                output.add(new ArrayList<>(res));
+                return;
+            }else return;
         }
-
+        res.add(nums[ind]);
         sum += nums[ind];
         //pick
-        int l = helper(nums, target, sum, ind+1);
+        helper(nums, target, sum, ind+1, output, res);
 
+        res.remove(res.size() - 1);
         sum -= nums[ind];
         //not pick
-        int r = helper(nums, target, sum, ind+1);
+        helper(nums, target, sum, ind+1, output, res);
 
-        return  l + r;
+
+        //to count we just simple add the recursive funcions
+        // return  l + r;
         
     }
     
